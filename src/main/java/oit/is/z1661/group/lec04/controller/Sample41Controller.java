6830680e96d6a2d2ffb68f6cd06a2d5e7aecdeab
e1b20069 <e1b20069@st.oit.ac.jp>
2022-10-18 16:34:10 +0900

    Sample4-6までの実装

--- src/main/java/oit/is/z1661/group/lec04/controller/Sample41Controller.java
+++ src/main/java/oit/is/z1661/group/lec04/controller/Sample41Controller.java
@@ -1,7 +1,11 @@
 package oit.is.z1661.group.lec04.controller;
 
+import java.security.Principal;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +28,16 @@ public class Sample41Controller {
     return "sample41.html";
   }
 
+  @GetMapping("step3")
+  public String sample43() {
+    return "sample43.html";
+  }
+
+  @GetMapping("step4")
+  public String sample45() {
+    return "sample45.html";
+  }
+
   /**
    * @param id
    * @param model
@@ -35,6 +49,25 @@ public class Sample41Controller {
     model.addAttribute("chamber2", chamber2);
 
     return "sample41.html";
+  }
+
+  @PostMapping("step3")
+  @Transactional
+  public String sample43(@RequestParam String chamberName, ModelMap model, Principal prin) {
+    String loginUser = prin.getName();
+    Chamber chamber3 = new Chamber();
+    chamber3.setChamberName(chamberName);
+    chamber3.setUserName(loginUser);
+    chamberMapper.insertChamber(chamber3);
+    model.addAttribute("chamber3", chamber3);
+    return "sample43.html";
+  }
+
+  @PostMapping("step5")
+  public String sample45(@RequestParam String chamberName, ModelMap model) {
+    ArrayList<Chamber> chambers5 = chamberMapper.selectAllByChamberName(chamberName);
+    model.addAttribute("chambers5", chambers5);
+    return "sample45.html";
   }
 
 }
