975fe28b06a65e0accf2c3b73647f8c1a9c4d95f
e1b20087 <e1b20087@oit.ac.jp>
2022-10-18 16:07:07 +0900

    sample4-2の実装

--- src/main/java/oit/is/z1661/group/lec04/controller/Sample41Controller.java
+++ src/main/java/oit/is/z1661/group/lec04/controller/Sample41Controller.java
@@ -1,5 +1,6 @@
 package oit.is.z1661.group.lec04.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +8,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.ui.ModelMap;
+import oit.is.z1661.group.lec04.model.Chamber;
+import oit.is.z1661.group.lec04.model.ChamberMapper;
+
 @Controller
 @RequestMapping("/sample4")
 public class Sample41Controller {
 
-  //@Autowired
-  //ChamberMapper chamberMapper;
+  @Autowired
+  ChamberMapper chamberMapper;
 
   @GetMapping("step1")
   public String sample41() {
+    return "sample41.html";
+  }
+
+  /**
+   * @param id
+   * @param model
+   * @return
+   */
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
     return "sample41.html";
   }
 
